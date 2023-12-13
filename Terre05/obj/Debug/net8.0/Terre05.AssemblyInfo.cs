923d6918b7bc88cc8e757f5b04e3b92168de8a92
Patrice Hanne <patrice.hanne@expleogroup.com>
2023-12-13 18:30:31 +0100

    ADD : T'as vu ?

--- Terre05/obj/Debug/net8.0/Terre05.AssemblyInfo.cs
+++ Terre05/obj/Debug/net8.0/Terre05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Terre05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b3b4d9d421eb6b4e34595b84ed7bc2b2629eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Terre05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Terre05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
