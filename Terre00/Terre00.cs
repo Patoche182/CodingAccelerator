fc1c33afcb53bf982818c6b17c03599e744aaf75
Patrice Hanne <patrice.hanne@expleogroup.com>
2023-12-13 16:44:36 +0100

    FIX : Big Bug

---
+++ Terre00/Terre00.cs
@@ -0,0 +1,35 @@
+// L’alphabet
+
+//Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
+
+//Exemples d’utilisation :
+//$> python exo.py
+//abcdefghijklmnopqrstuvwxyz
+//$>
+
+//Attention : votre programme devra utiliser une boucle.
+
+class Terre00
+{
+    static void Main()
+    {
+        string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        foreach (char lettre in lettreAlphabet)
+        {
+            Console.Write(lettre.ToString().ToLower());
+        }
+        Console.WriteLine();
+    }
+}
+
+//class Terre00
+//{
+//    static void Main()
+//    {
+//        for (char lettre = 'A'; lettre <= 'Z'; lettre++)
+//        {
+//            Console.Write(lettre.ToString().ToLower());
+//        }
+//        Console.WriteLine();
+//    }
+//}
