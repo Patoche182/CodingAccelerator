24b8fe17df957d98e07d2696b981125e2772e303
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-20 11:45:30 +0100

    ADD : PositifOuNegatif + MeF

--- Terre04/Terre04.cs
+++ Terre04/Terre04.cs
@@ -19,31 +19,55 @@ class Terre04
 {
     static void Main()
     {
+        Console.WriteLine("==========-==========-==========");
+        Console.WriteLine("Bonjour ! \nBienvenue dans ce programme. \nCette console récupère en [Entrée] le chiffre donné et renvoi en [Sortie] si c'est un chiffre Pair ou Impaire. \nIl renvoi aussi si il est Positif ou Négatif.");
+        Console.WriteLine("==========-==========-==========");
+        
+        // Boucle Do-While
         do
         {
-            Console.WriteLine("Donne un chiffre:");
+            Console.WriteLine("Donne-moi un chiffre:");
             string response = Console.ReadLine();
             int x;
-            if (int.TryParse(response, out x) && int.IsPositive(x))
+            if (int.TryParse(response, out x))
             {
-                Console.WriteLine(PairOuImpair(x));
+                Console.WriteLine("C'est un " + PairOuImpair(x) + " - " + PositifOuNegatif(x));
             }
             else
             {
-                Console.WriteLine("Tu ne me la mettras pas à l'envers.\nIl me faut un chiffre positif !");
+                Console.WriteLine("Tu ne me la mettras pas à l'envers. Il me faut un chiffre entier !");
             }
+            Console.WriteLine("---------- ---------- ----------");
         } while (true);
     }
 
+    // Voilà commment déterminer si l’argument donné est un entier pair ou impair
     static string PairOuImpair(int nombre)
     {
         if (nombre % 2 == 0)
         {
-            return "Pair !";
+            return "Pair";
         }
         else
         {
-            return "Impair !";
+            return "Impair";
+        }
+    }
+
+    // Voilà comment gérez aussi les entiers négatifs.
+    static string PositifOuNegatif(int nombre)
+    {
+        if (nombre > 0)
+        {
+            return "Positif";
+        }
+        else if (nombre < 0)
+        {
+            return "Négatif";
+        }
+        else
+        {
+            return "Zéro || 0 est bien Pair mais il n'est ni Positif ni Négatif !";
         }
     }
 }
