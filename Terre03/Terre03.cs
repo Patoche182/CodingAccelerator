dd2d7d5d4d06fbf47d34e0dd6d430efb0a73d4fc
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-20 14:22:42 +0100

    FIX :  Terre03

--- Terre03/Terre03.cs
+++ Terre03/Terre03.cs
@@ -11,20 +11,75 @@ class Terre03
 {
     static void Main()
     {
-        GetAlphabetFromLetter('P');
+        Console.WriteLine("==========-==========-==========");
+        Console.WriteLine("Bonjour ! \nBienvenue dans ce programme. \nCette console donne l'alphabet à partir de la lettre donnée en argument.");
+        Console.WriteLine("==========-==========-==========");
+
+        do
+        {
+            Console.Write("Veuillez entrer une lettre : ");
+            string userInput = Console.ReadLine();
+            if (userInput.Length == 1 && char.IsLetter(userInput[0]))
+            {
+                char userLetter = char.ToLower(userInput[0]);
+                GetAlphabetFromLetter(userLetter);
+            }
+            else
+            {
+                Console.WriteLine("Entrée non valide. Vous avez entrez un chiffre ou plusieurs caractères.");
+            }
+        } while (true);
     }
 
     static void GetAlphabetFromLetter(char letter)
     {
-        letter = Convert.ToChar(letter.ToString().ToLower());
-        string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        foreach (char i in lettreAlphabet.ToLower())
+        string alphabet = "abcdefghijklmnopqrstuvwxyz"; // abcdefghijklmnopqrstuvwxyz "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        letter = char.ToLower(letter);
+        bool found = false; // J'intègre un bool ==> 
+        /*
+         La variable found est une variable booléenne (de type bool) qui est utilisée comme un indicateur pour suivre si la lettre de l'utilisateur a été trouvée lors de la boucle à travers l'alphabet.
+         Son objectif est de déterminer à partir de quelle position dans l'alphabet l'affichage doit commencer.
+         Initialement, found est défini sur false (non trouvé) avant que la boucle commence.
+         Pendant la boucle, lorsque la lettre de l'utilisateur est trouvée (c'est-à-dire lorsque letter == i), la variable found est définie sur true pour indiquer que la lettre a été trouvée.
+         Ensuite, la boucle continue à afficher les caractères de l'alphabet même après avoir trouvé la lettre de l'utilisateur.
+         Cette approche permet de déterminer à partir de quelle position dans l'alphabet l'affichage doit commencer, garantissant ainsi que la lettre de l'utilisateur est incluse dans le résultat.
+         Sans cette variable, l'affichage commencerait dès le début de l'alphabet, et la lettre de l'utilisateur ne serait pas incluse dans le résultat.
+         */
+
+        foreach (char i in alphabet)
         {
-            if (letter < i)
+            if (letter == i)
             {
-            Console.Write(i);
+                found = true;
+            }
+            if (found)
+            {
+                Console.Write(i);
             }
         }
-        Console.WriteLine();
+        Console.WriteLine("\n-------------\n");
     }
 }
+
+//static void Main()
+//{
+//    Console.WriteLine("==========-==========-==========");
+//    Console.WriteLine("Bonjour ! \nBienvenue dans ce programme. \nCette console donne l'alphabet à partir de la lettre donnée en argument.");
+//    Console.WriteLine("==========-==========-==========");
+
+//    GetAlphabetFromLetter('F');
+//}
+
+//static void GetAlphabetFromLetter(char letter)
+//{
+//    letter = Convert.ToChar(letter.ToString().ToLower());
+//    string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//    foreach (char i in lettreAlphabet.ToLower())
+//    {
+//        if (letter < i)
+//        {
+//        Console.Write(i);
+//        }
+//    }
+//    Console.WriteLine();
+//}
