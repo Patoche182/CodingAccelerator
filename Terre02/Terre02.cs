1844b300391615630b2001f3a235ed7629521674
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-20 16:17:49 +0100

    FIX : Methode 1 et 2

--- Terre02/Terre02.cs
+++ Terre02/Terre02.cs
@@ -9,10 +9,8 @@ Exemples d’utilisation :
     !
 */
 
-
 class Terre02
 {
-
     class Program
     {
         static void Main()
@@ -21,53 +19,59 @@ class Terre02
             Console.WriteLine("Bonjour ! \nBienvenue dans ce programme. \nCette console affiche les arguments reçus ligne par ligne.");
             Console.WriteLine("==========-==========-==========");
 
-            string[] args = {"Je s'appelle Groot !", "Je suis Patrice...", "\\m/_Children_Of_Bodom COB"};
-            GetArgs(args);
+            //// Methode 1:
+
+            //            string[] args = {"Je s'appelle Groot !", "Je suis Patrice...", "\\m/_Children_Of_Bodom COB"};
+            //            GetArgs(args);
+            //        }
+
+            //        static void GetArgs(string[] args)
+            //        {
+            //            for (int i = 0; i < args.Length; i++)
+            //            {
+            //                string[] splitArgs = args[i].Split(' ');
+
+            //                for (int j = 0; j < splitArgs.Length; j++)
+            //                {
+            //                    Console.WriteLine(splitArgs[j]);
+            //                }
+            //            }
+            //            Console.WriteLine("---------- ---------- ----------");
+            //            Console.WriteLine();
+            //        }
+            //    }
+            //}
+
+            // Methode 2:
+
+            do
+            {
+                Console.WriteLine("Veuillez entrer une phrase (ou 'exit' pour quitter) :");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                    break;
+
+                string[] phraseInput = { input };
+                Console.WriteLine("---------- ---------- ----------");
+                ConvertirPhrase(phraseInput);
+                Console.WriteLine("---------- ---------- ----------");
+            } while (true);
         }
 
-        static void GetArgs(string[] args)
+        static void ConvertirPhrase(string[] phraseInput)
         {
-            for (int i = 0; i < args.Length; i++)
+            for (int mot = 0; mot < phraseInput.Length; mot++)
             {
-                string[] splitArgs = args[i].Split(' ');
+                // Utilisez la méthode Split pour diviser la chaîne par les espaces
+                string[] splitPhraseInput = phraseInput[mot].Split(' ');
 
-                for (int j = 0; j < splitArgs.Length; j++)
+                // Affichez chaque élément de la chaîne divisée ligne par ligne
+                for (int mots = 0; mots < splitPhraseInput.Length; mots++)
                 {
-                    Console.WriteLine(splitArgs[j]);
+                    Console.WriteLine(splitPhraseInput[mots].Trim()); // Utilisez Trim pour supprimer les espaces autour des mots
                 }
             }
             Console.WriteLine();
         }
     }
 }
-
-//            do
-//            {
-//                Console.WriteLine("Entrez une chaîne de caractères (ou 'exit' pour quitter) :");
-//                string input = Console.ReadLine();
-//                if (input.ToLower() == "exit")
-//                    break;
-//                string[] phraseInput = { input };
-//                Console.WriteLine("---------- ---------- ----------");
-//                GetArgs(phraseInput);
-//                Console.WriteLine("---------- ---------- ----------");
-//            } while (true);
-//        }
-
-//        static void GetArgs(string[] phraseInput)
-//        {
-//            for (int i = 0; i < phraseInput.Length; i++)
-//            {
-//                // Utilisez la méthode Split pour diviser la chaîne par les espaces
-//                string[] splitPhraseInput = phraseInput[i].Split(' ');
-
-//                // Affichez chaque élément de la chaîne divisée ligne par ligne
-//                for (int j = 0; j < splitPhraseInput.Length; j++)
-//                {
-//                    Console.WriteLine(splitPhraseInput[j].Trim()); // Utilisez Trim pour supprimer les espaces autour des mots
-//                }
-//            }
-//            Console.WriteLine();
-//        }
-//    }
-//}
