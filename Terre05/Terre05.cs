3bf1185c938c68d7769c921fa2debf19c4d4bfa5
Patrice Hanne <patrice.hanne@expleogroup.com>
2024-01-05 18:26:06 +0100

    ADD : Division

--- Terre05/Terre05.cs
+++ Terre05/Terre05.cs
@@ -11,3 +11,34 @@ Exemples d’utilisation :
     erreur.
 */
 // Va faloir le commencer à m'en donné !
+
+class Terre05
+{
+    // exemple 5/2 = 2 : % = 1
+
+    static void Main()
+    {
+        int a = 10;
+        int b = 3;
+        Console.WriteLine(Division(a, b));
+        Console.WriteLine(Reste(a, b));
+    }
+
+    static int Division(int a, int b)
+    {
+        int resultat = a / b;
+        return resultat;
+    }
+
+    static int Reste(int a, int b)
+    {
+        int reste = a % b;
+        return reste;
+    }
+
+    //static (int, int) Reste(int a, int b)
+    //{
+    //    int reste = a % b;
+    //    return (resultat, reste);
+    //}
+}
