97936206cc953bd7a7b8845b45f8f25e44e258b6
Patrice Hanne <patrice.hanne@expleogroup.com>
2024-01-05 18:43:20 +0100

    ADD : Terre06

--- Terre06/Terre06.cs
+++ Terre06/Terre06.cs
@@ -11,3 +11,22 @@ Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments.
 Fonctions interdites: 
 -La fonction reverse
 */
+
+class Terre06
+{
+    static void Main()
+    {
+        string chaine = "Je s'apelle Groot !";
+        Console.WriteLine(Reverse(chaine));
+    }
+    static string Reverse(string chaine)
+    {
+        string inverse = "";
+        for (int i = chaine.Length - 1; i >= 0; i--)
+        {
+            // écrire les char un à un dans inverse
+            inverse += chaine[i];
+        }
+        return inverse;
+    }
+}
