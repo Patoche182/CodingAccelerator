12c6119c3c8e27f440c33c0b014e6e0830fa8ed6
PatriceHANNE <patrice.hanne@gmail.com>
2024-01-06 01:36:27 +0100

    ADD : Projet suivant

---
+++ Terre07/Terre07.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Terre07
+{
+    static void Main()
+    {
+        string chaine = "Je s'apelle Groot 22 !";
+        Console.WriteLine(Reverse(chaine));
+    }
+    static string Reverse(string chaine)
+    {
+        string inverse = "";
+        for (int i = chaine.Length - 1; i >= 0; i--)
+        {
+            // écrire les char un à un dans inverse
+            inverse += chaine[i];
+        }
+        return inverse;
+    }
+}
