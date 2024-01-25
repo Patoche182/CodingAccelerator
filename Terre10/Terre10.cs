1138451c5ae14ed08274f9ee5885691f6822b203
PatriceHANNE <patrice.hanne@gmail.com>
2024-01-25 01:11:42 +0100

    ADD : Soluss

--- Terre10/Terre10.cs
+++ Terre10/Terre10.cs
@@ -19,81 +19,64 @@ class Terre10
         Console.WriteLine("Cette console affiche si le nombres donné est en Nombre Premier.");
         Console.WriteLine("==========-==========-==========-==========-==========");
 
-        /* 
-            NOMBRE PREMIER
-        Nombre naturel qui est supérieur à 1 et qui a exactement deux diviseurs distincts, soit 1 et lui-même.
-        Il existe une infinité de nombres premiers.
-        Par définition, les nombres 0 et 1 ne sont ni premiers, ni composés.
-        Un nombre naturel supérieur à 1 qui n’est pas premier est un nombre composé et vice versa.
-        Tout nombre composé peut être exprimé d’une façon unique sous la forme d’un produit de nombres premiers.
 
-            EXEMPLE
-        Le nombre 13 est un nombre premier car il n’est divisible que par 1 et 13.
-        Voici la liste des 15 nombres premiers inférieurs à  50 : 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47.
-        Le nombre 15 n’est pas un nombre premier, car il a plus de deux diviseurs : div (15) = {1, 3, 5, 15}.
-        Le nombre 9 n’est pas un nombre premier, car il a plus de deux diviseurs : div (9) = {1, 3, 9}. 
-         */
+        /* NOMBRE PREMIER
+           Nombre naturel qui est supérieur à 1 et qui a exactement deux diviseurs distincts, soit 1 et lui-même.
+           Il existe une infinité de nombres premiers.
+
+           EXEMPLE
+           Le nombre 13 est un nombre premier car il n’est divisible que par 1 et 13.
+           Voici la liste des 15 nombres premiers inférieurs à  50 : 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47.
+           Le nombre 15 n’est pas un nombre premier, car il a plus de deux diviseurs : div (15) = {1, 3, 5, 15}.
+           Le nombre 9 n’est pas un nombre premier, car il a plus de deux diviseurs : div (9) = {1, 3, 9}.    */
+
 
         do
         {
-            Console.WriteLine("Veuillez entrer un nombre : (ou 'exit' pour quitter).");
-            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un nombre entier positif (ou 'exit' pour quitter) :");
+            string input = Console.ReadLine();
 
-            bool isPrime = EstNombrePremier(number);
+            if (input.ToLower() == "exit")
+                break;
 
-            if (isPrime)
+            if (int.TryParse(input, out int number) && number > 0)
             {
-                Console.WriteLine($"{number} est un nombre premier.");
+                bool isPrime = EstNombrePremier(number);
+
+                if (isPrime)
+                {
+                    Console.WriteLine($"Oui, {number} est un nombre premier.");
+                }
+                else
+                {
+                    Console.WriteLine($"Non, {number} n'est pas un nombre premier.");
+                }
             }
             else
             {
-                Console.WriteLine($"{number} n'est pas un nombre premier.");
+                Console.WriteLine("Veuillez entrer un nombre entier positif.");
             }
-            //else
-            //{
-            //    Console.WriteLine("- /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ -");
-            //    Console.WriteLine("- Attention ! Veuillez entrer un nombre ENTIER et POSITIF svp -");
-            //    Console.WriteLine("- /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ -");
-            //}
             Console.WriteLine("----------=----------=----------=----------=----------");
-
         } while (true);
     }
 
-    // Méthodes :
     static bool EstNombrePremier(int n)
     {
-        // TODO: Implémenter la logique pour déterminer si n est un nombre premier
-        // N'oubliez pas de gérer les cas particuliers (0, 1, etc.)
-        return false;  // Placeholder, changez cela avec votre propre logique
+        if (n <= 1)
+        {
+            // 0 et 1 ne sont pas des nombres premiers
+            return false;
+        }
+        // Vérifier si n est divisible par un nombre de 2 à la racine carrée de n
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                // n est divisible par i, donc ce n'est pas un nombre premier
+                return false;
+            }
+        }
+        // Si aucun diviseur n'est trouvé, n est un nombre premier
+        return true;
     }
 }
-
-/*
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Veuillez entrer un nombre entier positif :");
-        int number = Convert.ToInt32(Console.ReadLine());
-
-        bool isPrime = EstNombrePremier(number);
-
-        if (isPrime)
-        {
-            Console.WriteLine($"{number} est un nombre premier.");
-        }
-        else
-        {
-            Console.WriteLine($"{number} n'est pas un nombre premier.");
-        }
-    }
-
-    static bool EstNombrePremier(int n)
-    {
-        // TODO: Implémenter la logique pour déterminer si n est un nombre premier
-        // N'oubliez pas de gérer les cas particuliers (0, 1, etc.)
-        return false;  // Placeholder, changez cela avec votre propre logique
-    }
-}
-*/
