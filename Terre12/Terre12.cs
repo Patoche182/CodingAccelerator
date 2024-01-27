6a7a9659eb3ce7f1f5cfd75ce6fb527946c49e11
PatriceHANNE <patrice.hanne@gmail.com>
2024-01-27 01:20:52 +0100

    pmppm

--- Terre12/Terre12.cs
+++ Terre12/Terre12.cs
@@ -10,10 +10,48 @@ Attention : midi et minuit.
 */
 
 
-namespace Terre12
+class Terre12
 {
-    internal class Terre12
+    static void Main(string[] args)
     {
-        // allé on va le faire et vite :) !!!
+        Console.WriteLine("==========-==========-==========-==========-==========");
+        Console.WriteLine("Bonjour ! \nBienvenue dans ce programme.");
+        Console.WriteLine("Cette console transforme une heure affichée en format 12h en une heure affichée en format 24h.");
+        Console.WriteLine("==========-==========-==========-==========-==========");
+        string input;
+        do
+        {
+            Console.Write("Entrez l'heure au format 12h (HH:mm) : ");
+            input = Console.ReadLine();
+
+            if (DateTime.TryParseExact(input, "HH:mm", null, System.Globalization.DateTimeStyles.None, out DateTime time))
+            {
+                string formattedTime = ConvertTo12HourFormat(time);
+                Console.WriteLine("Heure en format 12h : " + formattedTime);
+            }
+            else
+            {
+                Console.WriteLine("Format d'heure incorrect. Assurez-vous d'utiliser le format HH:mm.");
+            }
+
+            Console.Write("Voulez-vous continuer? (O/N) : ");
+        } while (true);
+    }
+
+    static string ConvertTo12HourFormat(DateTime time)
+    {
+        string period = "AM";
+        if (time.Hour >= 12)
+        {
+            period = "PM";
+        }
+
+        int hour12 = time.Hour > 12 ? time.Hour - 12 : time.Hour;
+        if (hour12 == 0)
+        {
+            hour12 = 12; // Pour 00:00 en format 12h, nous affichons 12 AM.
+        }
+
+        return $"{hour12:D2}:{time.Minute:D2} {period}";
     }
 }
