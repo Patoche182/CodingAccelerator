b32e756a898aa375b8b7e1607e9baf19a76d8585
PatriceHANNE <patrice.hanne@gmail.com>
2024-02-04 18:40:33 +0100

    Correction

--- Terre15/Terre15.cs
+++ Terre15/Terre15.cs
@@ -21,19 +21,3 @@ class Terre15
         Console.ReadKey();
     }
 }
-
-Créez un programme qui affiche toutes les différentes combinaisons possibles de trois chiffres dans l’ordre croissant, dans l’ordre croissant. La répétition est volontaire.
-
-
-Exemples d’utilisation :
-$> python exo.py
-012, 013, 014, 015, 016, 017, 018, 019, 023, 024, ... , 789
-$>
-
-987 n’est pas là parce que 789 est présent.
-
-020 n’est pas là parce que 0 apparaît deux fois.
-
-021 n’est pas là parce que 012 est présent.
-
-000 n’est pas là parce que cette combinaison ne comporte pas exclusivement des chiffres différents les uns des autres.
