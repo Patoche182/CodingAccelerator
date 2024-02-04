1fb65d4190f7ed089d8915811ac45445556541b1
PatriceHANNE <patrice.hanne@gmail.com>
2024-02-04 18:11:18 +0100

    ADD Air

---
+++ Air00/Air00.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Air00
+{
+    internal class Air00
+    {
+    }
+}
