ea3a1383d97d1de8ac484341b769efcd279d7c96
Patrice Hanne <patrice.hanne@expleogroup.com>
2024-02-22 00:57:34 +0100

    Eau01 start

--- Eau01/Eau01.cs
+++ Eau01/Eau01.cs
@@ -14,8 +14,34 @@ class Eau01
 {
     static void Main()
     {
-        Console.WriteLine("Epreuve de l'Eau !");
         Console.WriteLine("==========-==========-==========-==========-==========");
-        Console.ReadKey();
+        Console.WriteLine("Epreuve de l'Eau01 !");
+        Console.WriteLine("==========-==========-==========-==========-==========");
+
+
+
+
     }
 }
+
+/*
+ int centaine = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            int dizaine = 0;
+            for (int j = 0; j < 10; j++)
+            {
+                int unite = 0;
+                for (int k = 0; k < 10; k++)
+                {
+                    if ((unite != dizaine) && (dizaine != centaine) && (unite != centaine) && (unite > dizaine) && (dizaine > centaine))
+                    {
+                        Console.WriteLine($"{centaine}" + $"{dizaine}" + $"{unite}");
+                    }
+                    unite = unite + 1;
+                }
+                dizaine = dizaine + 1;
+            }
+            centaine = centaine + 1;
+        } 
+ */
